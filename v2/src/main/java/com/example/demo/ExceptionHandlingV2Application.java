08f99aa9b83ef5be5b777bbfebab56589495f621
hanyeong0815 <yoehadu815@gmail.com>
2023-05-17 17:27:47 +0900

    chore: rename application java

--- v2/src/main/java/com/example/demo/ExceptionHandlingV2Application.java
+++ v2/src/main/java/com/example/demo/ExceptionHandlingV2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class V2Application {
+public class ExceptionHandlingV2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(V2Application.class, args);
+        SpringApplication.run(ExceptionHandlingV2Application.class, args);
     }
 
 }
